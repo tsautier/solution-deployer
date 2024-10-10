81336664aad9943f5e6bde58a3e92271f30ef32c
Dmitry Perets <dperets@fortinet.com>
2024-10-10 20:30:28 +0200

    combine lab instance config scripts

---
+++ configure_instance.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+status_fail=0
+
+# Configure external NAT
+./configure_ext_nat.py || status_fail=1
+
+# Finalize FGT configuration
+./configure_devices.py || status_fail=1
+
+# Update public IPs in Postman variables (CustomerU)
+./update_postman_vars.py || status_fail=1
+
+exit $status_fail
